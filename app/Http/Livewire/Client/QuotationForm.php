e4482b8bce4a38bf395923fa7d49c02b6e4eb375
Godwin Elitcha <godwin.elitcha@gmail.com>
2021-12-21 17:29:23 +0000

    Feat: Setup Client and quotation management on admin panel

--- app/Http/Livewire/Client/QuotationForm.php
+++ app/Http/Livewire/Client/QuotationForm.php
@@ -103,6 +103,10 @@ class QuotationForm extends Component
     }
     public function mount()
     {
+        //Through exception
+        $this->quotation = Quotation::findOrFail(1);
+        return;
+
         $this->items = [$this->makeBlankItem()];
         $this->quotation = $this->makeBlankQuotation();
     }
