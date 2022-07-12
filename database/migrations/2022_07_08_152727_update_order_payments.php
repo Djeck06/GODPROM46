31bbd5e12774f97a00646022a07c2a3052f98dce
unknown <harry.kouevi@gmail.com>
2022-07-12 18:02:06 +0000

    migration correction

--- database/migrations/2022_07_08_152727_update_order_payments.php
+++ database/migrations/2022_07_08_152727_update_order_payments.php
@@ -13,20 +13,52 @@ class UpdateOrderPayments extends Migration
      */
     public function up()
     {
-        Schema::table('order_payments', function (Blueprint $table) {
-           
-            //$table->string('status')->default('pending'); // pending | succeeded | canceled
-         
-            $table->softDeletes();
-            $table->timestamps();
-            $table->unsignedBigInteger('payment_platform_id')->nullable()->change();
-            $table->string('account_number')->nullable()->change();
-            $table->string('stripe_intent_id');
+        
+        if (!Schema::hasColumn('order_payments','deleted_at')) {
+            Schema::table('order_payments', function (Blueprint $table) {
+
+                $table->softDeletes();
+            });
+        }
+
+        if (!Schema::hasColumn('order_payments','created_at')) {
+            Schema::table('order_payments', function (Blueprint $table) {
+
+                $table->timestamps();
+            });
+        }
+
+        if (!Schema::hasColumn('order_payments','payment_platform_id')) {
+            Schema::table('order_payments', function (Blueprint $table) {
+
+                $table->unsignedBigInteger('payment_platform_id')->nullable()->change();
+            });
+        }
+
+        if (!Schema::hasColumn('order_payments','stripe_intent_id')) {
+            Schema::table('order_payments', function (Blueprint $table) {
+
+                $table->string('stripe_intent_id')->nullable();
+            });
+        }
+
+        
 
 
+        if (!Schema::hasColumn('order_payments','account_number')) {
+            Schema::table('order_payments', function (Blueprint $table) {
 
+                $table->string('account_number')->nullable();
 
-        });
+            });
+        }else{
+            Schema::table('order_payments', function (Blueprint $table) {
+
+                $table->string('account_number')->nullable()->change();
+
+            });
+            
+        }
 
         $records= \App\Models\Order::where('delivery_country',Null)->get();
         foreach($records as $order){
